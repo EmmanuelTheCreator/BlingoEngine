b6c373fb9003c4398e610ca092bca249f3a7439a
Emmanuel <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-07-22 11:23:46 +0200

    Fix namespaces

--- src/Director/LingoEngine.Director.Core/Stages/StageBoundingBoxesOverlay.cs
+++ src/Director/LingoEngine.Director.Core/Stages/StageBoundingBoxesOverlay.cs
@@ -7,6 +7,9 @@ using LingoEngine.Gfx;
 using LingoEngine.Primitives;
 using LingoEngine.Sprites;
 using LingoEngine.Inputs;
+using LingoEngine.Director.Core.Tools;
+using LingoEngine.Events;
+using LingoEngine.Texts;
 
 namespace LingoEngine.Director.Core.Stages;
 
