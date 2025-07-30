f89b1b814e46c7de7974a178fbcd72464993ad2e
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-07-30 08:46:08 +0200

    Add AddFromSprites helper

--- src/LingoEngine/Bitmaps/LingoMemberFilmLoop.cs
+++ src/LingoEngine/Bitmaps/LingoMemberFilmLoop.cs
@@ -1,7 +1,11 @@
 using LingoEngine.Casts;
 using LingoEngine.FilmLoops;
+using LingoEngine.Sounds;
+using LingoEngine.Sprites;
 using LingoEngine.Members;
 using LingoEngine.Primitives;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LingoEngine.Bitmaps
 {
@@ -13,25 +17,15 @@ namespace LingoEngine.Bitmaps
         private readonly ILingoFrameworkMemberFilmLoop _frameworkFilmLoop;
 
         /// <summary>
-        /// The sequence of cast members that make up this film loop. Each entry
-        /// references a cast library number and member number pair.
+        /// Timeline data describing the sprites and sounds composing this film loop.
         /// </summary>
-        public List<LingoFilmLoopFrameRef> Frames { get; } = new();
+        public LingoFilmLoop FilmLoop { get; } = new();
 
         /// <summary>
         /// Gets the framework specific implementation for this film loop.
         /// </summary>
         public T Framework<T>() where T : class, ILingoFrameworkMemberFilmLoop => (T)_frameworkFilmLoop;
 
-        /// <summary>
-        /// The media data representing the frames and channels selection.
-        /// Lingo: the media of member
-        /// </summary>
-        public byte[]? Media
-        {
-            get => _frameworkFilmLoop.Media;
-            set => _frameworkFilmLoop.Media = value;
-        }
 
         /// <summary>
         /// How the film loop content is framed within the sprite rectangle.
@@ -65,5 +59,52 @@ namespace LingoEngine.Bitmaps
         public override void ImportFileInto() => _frameworkFilmLoop.ImportFileInto();
         public override void CopyToClipBoard() => _frameworkFilmLoop.CopyToClipboard();
         public override void PasteClipBoardInto() => _frameworkFilmLoop.PasteClipboardInto();
+
+        /// <summary>
+        /// Adds a sprite to the film loop timeline.
+        /// </summary>
+        /// <param name="channel">Sprite channel inside the film loop.</param>
+        /// <param name="beginFrame">First frame on which the sprite is shown.</param>
+        /// <param name="endFrame">Last frame on which the sprite is shown.</param>
+        /// <param name="sprite">The sprite to add.</param>
+        public void AddSprite(int channel, int beginFrame, int endFrame, LingoSprite2D sprite)
+            => FilmLoop.AddSprite(channel, beginFrame, endFrame, sprite);
+
+        /// <summary>
+        /// Adds a sound to one of the film loop audio channels.
+        /// </summary>
+        /// <param name="channel">Audio channel index (1 or 2).</param>
+        /// <param name="startFrame">Frame at which the sound should start.</param>
+        /// <param name="sound">Sound member to play.</param>
+        public void AddSound(int channel, int startFrame, LingoMemberSound sound)
+            => FilmLoop.AddSound(channel, startFrame, sound);
+
+        /// <summary>
+        /// Populate the film loop timeline from a list of existing sprites.
+        /// The method normalizes the channel and frame numbers so that the
+        /// earliest sprite begins on frame 1 and the lowest channel becomes
+        /// channel 1.
+        /// </summary>
+        /// <param name="sprites">Sprites to import into the film loop.</param>
+        public void AddFromSprites(IEnumerable<LingoSprite2D> sprites)
+        {
+            if (sprites == null)
+                return;
+
+            var list = sprites.ToList();
+            if (list.Count == 0)
+                return;
+
+            int minFrame = list.Min(s => s.BeginFrame);
+            int minChannel = list.Min(s => s.SpriteNum);
+
+            foreach (var sp in list)
+            {
+                int channel = sp.SpriteNum - minChannel + 1;
+                int begin = sp.BeginFrame - minFrame + 1;
+                int end = sp.EndFrame - minFrame + 1;
+                AddSprite(channel, begin, end, sp);
+            }
+        }
     }
 }
---
+++ src/LingoEngine/FilmLoops/LingoFilmLoop.cs
@@ -0,0 +1,39 @@
+namespace LingoEngine.FilmLoops;
+
+using LingoEngine.Sounds;
+using LingoEngine.Sprites;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Represents the timeline data of a film loop. It describes layered sprites
+/// and sounds with their own mini timeline.
+/// </summary>
+public class LingoFilmLoop
+{
+    /// <summary>
+    /// Information about a sprite used in the film loop timeline.
+    /// </summary>
+    public record SpriteEntry(int Channel, int BeginFrame, int EndFrame, LingoSprite2D Sprite);
+
+    /// <summary>
+    /// Description of a sound placed on one of the two audio channels.
+    /// </summary>
+    public record SoundEntry(int Channel, int StartFrame, LingoMemberSound Sound);
+
+    public List<SpriteEntry> SpriteEntries { get; } = new();
+    public List<SoundEntry> SoundEntries { get; } = new();
+
+    public int FrameCount { get; private set; }
+
+    public void AddSprite(int channel, int beginFrame, int endFrame, LingoSprite2D sprite)
+    {
+        SpriteEntries.Add(new SpriteEntry(channel, beginFrame, endFrame, sprite));
+        FrameCount = Math.Max(FrameCount, endFrame);
+    }
+
+    public void AddSound(int channel, int startFrame, LingoMemberSound sound)
+    {
+        SoundEntries.Add(new SoundEntry(channel, startFrame, sound));
+    }
+}
