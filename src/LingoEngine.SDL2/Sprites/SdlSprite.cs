7d2c93557a4cabf8ed5fa405a248c9e3294adbe3
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-08-13 23:12:33 +0200

    Fix SDL sprite registration point offset

--- src/LingoEngine.SDL2/Sprites/SdlSprite.cs
+++ src/LingoEngine.SDL2/Sprites/SdlSprite.cs
@@ -232,8 +232,8 @@ public class SdlSprite : ILingoFrameworkSprite, ILingoSDLComponent, IDisposable
             }
         }
 
-        ComponentContext.OffsetX = offset.X;
-        ComponentContext.OffsetY = offset.Y;
+        ComponentContext.OffsetX = -offset.X;
+        ComponentContext.OffsetY = -offset.Y;
         UpdateContextPosition();
         return _texture;
     }
@@ -359,13 +359,13 @@ public class SdlSprite : ILingoFrameworkSprite, ILingoSDLComponent, IDisposable
     private void ApplyBlend()
     {
         //ComponentContext.Blend = _directToStage ? 1f : _blend;
-        ComponentContext.Blend =  _directToStage ? 1f : _blend/100;
+        ComponentContext.Blend = _directToStage ? 1f : _blend / 100;
     }
 
     private void UpdateContextPosition()
     {
-        int x = (int)(_x - ComponentContext.OffsetX - ComponentContext.TargetWidth / 2f);
-        int y = (int)(_y - ComponentContext.OffsetY - ComponentContext.TargetHeight / 2f);
+        int x = (int)(_x - ComponentContext.TargetWidth / 2f);
+        int y = (int)(_y - ComponentContext.TargetHeight / 2f);
         ComponentContext.X = x;
         ComponentContext.Y = y;
     }
