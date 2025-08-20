cdaba5e2bb59782542460a397b526a0e8b4e342c
Emmanuel The Creator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-08-20 18:51:47 +0200

    Sync channel visibility with score mute button

--- src/Director/LingoEngine.Director.Core/Scores/DirScoreChannelHeader.cs
+++ src/Director/LingoEngine.Director.Core/Scores/DirScoreChannelHeader.cs
@@ -10,19 +10,19 @@ namespace LingoEngine.Director.Core.Scores
     {
         private AColor _textColor = AColor.FromHex("#a0a0a0");
         protected readonly DirScoreGfxValues _gfxValues;
-        private readonly Action<DirScoreChannelHeader,bool> _muteStateChanged;
+        private readonly Action<DirScoreChannelHeader, bool> _muteStateChanged;
 
         public virtual string Icon { get; set; }
         public virtual string Label { get; set; }
         private bool _muted;
         public bool IsMuted => _muted;
 
-        public int Width { get; private set; }  
-        public int Height { get; private set; }  
+        public int Width { get; private set; }
+        public int Height { get; private set; }
         public float Y { get; set; }
         public bool Visible { get; internal set; } = true;
 
-        public DirScoreChannelHeader(string icon, string label, DirScoreGfxValues gfxValues, Action<DirScoreChannelHeader,bool> muteStateChanged)
+        public DirScoreChannelHeader(string icon, string label, DirScoreGfxValues gfxValues, Action<DirScoreChannelHeader, bool> muteStateChanged)
         {
             _gfxValues = gfxValues;
             _muteStateChanged = muteStateChanged;
@@ -36,20 +36,20 @@ namespace LingoEngine.Director.Core.Scores
         {
             var xOffset = 1;
             var y = Y;
-            
 
-            canvas.DrawText(new APoint(xOffset+20 + 25, y+ 11), Icon,null, _textColor, 10);
+
+            canvas.DrawText(new APoint(xOffset + 20 + 25, y + 11), Icon, null, _textColor, 10);
             if (!string.IsNullOrEmpty(Label))
-                canvas.DrawText(new APoint(xOffset+20 + 2, y+ 11), Label, null, _textColor, 10,45, AbstUI.Texts.AbstTextAlignment.Right);
+                canvas.DrawText(new APoint(xOffset + 20 + 2, y + 11), Label, null, _textColor, 10, 45, AbstUI.Texts.AbstTextAlignment.Right);
 
             // Visibility rect
             var btnWidth = 8;
             var btnHeight = 8;
-            var btnLeft = xOffset+5;
-            var btnTop = y+ 4;
+            var btnLeft = xOffset + 5;
+            var btnTop = y + 4;
             if (_muted)
             {
-                canvas.DrawRect(new ARect(btnLeft, btnTop, btnLeft+ btnWidth, btnTop+btnHeight), DirectorColors.LineDarker);
+                canvas.DrawRect(new ARect(btnLeft, btnTop, btnLeft + btnWidth, btnTop + btnHeight), DirectorColors.LineDarker);
                 canvas.DrawLine(new APoint(btnLeft, btnTop + btnHeight), new APoint(btnLeft + btnWidth, btnTop + btnHeight), DirectorColors.LineLight);  // bottom line
                 canvas.DrawLine(new APoint(btnLeft + btnWidth, btnTop), new APoint(btnLeft + btnWidth, btnTop + btnHeight), DirectorColors.LineLight); // right line
             }
@@ -76,14 +76,16 @@ namespace LingoEngine.Director.Core.Scores
             // Vertical line at the end
             canvas.DrawLine(new APoint(Width, y), new APoint(Width, y + _gfxValues.ChannelHeight), _gfxValues.ColLineDark);
 
-           
+
         }
-        public void SetMuted(bool state)
+        public void SetMuted(bool state, bool notify = true)
         {
             _muted = state;
-            _muteStateChanged(this, state);
+            if (notify)
+                _muteStateChanged(this, state);
             OnMutedChanged(state);
         }
+        public void SetMutedExternal(bool state) => SetMuted(state, false);
         protected virtual void OnMutedChanged(bool state) { }
         public void ToggleMute() => SetMuted(!_muted);
     }
