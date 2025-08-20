896211c7811f8458df1695141590904f17baf1d2
Emmanuel The Creator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-08-20 22:56:04 +0200

    Add SDL2 property inspector window

---
+++ src/Director/LingoEngine.Director.SDL2/Inspector/DirSdlPropertyInspectorWindow.cs
@@ -0,0 +1,39 @@
+using AbstUI.Components;
+using AbstUI.SDL2.Components;
+using AbstUI.SDL2.Components.Containers;
+using LingoEngine.Director.Core.Inspector;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LingoEngine.Director.SDL2.Inspector;
+
+internal class DirSdlPropertyInspectorWindow : AbstSdlWindow, IDirFrameworkPropertyInspectorWindow
+{
+    private readonly DirectorPropertyInspectorWindow _directorInspectorWindow;
+    private readonly AbstSdlPanel _headerPanel;
+    private readonly AbstSdlTabContainer _tabs;
+    private const int TitleBarHeight = 24;
+
+    public DirSdlPropertyInspectorWindow(DirectorPropertyInspectorWindow directorInspectorWindow, IServiceProvider services)
+        : base((AbstSdlComponentFactory)services.GetRequiredService<IAbstComponentFactory>())
+    {
+        _directorInspectorWindow = directorInspectorWindow;
+        Init(_directorInspectorWindow);
+        _directorInspectorWindow.Init(TitleBarHeight);
+
+        _headerPanel = _directorInspectorWindow.HeaderPanel.Framework<AbstSdlPanel>();
+        _tabs = _directorInspectorWindow.Tabs.Framework<AbstSdlTabContainer>();
+
+        _headerPanel.Y = TitleBarHeight;
+        _headerPanel.Width = Width;
+
+        _tabs.Y = TitleBarHeight + _headerPanel.Height;
+        _tabs.Width = Width;
+        _tabs.Height = Height - TitleBarHeight - _headerPanel.Height;
+
+        AddItem(_headerPanel);
+        AddItem(_tabs);
+
+        _directorInspectorWindow.ResizeFromFW(true, (int)Width, (int)Height - TitleBarHeight - (int)_headerPanel.Height);
+    }
+}
+
--- src/Director/LingoEngine.Director.SDL2/UI/LingoSdlDirectorRoot.cs
+++ src/Director/LingoEngine.Director.SDL2/UI/LingoSdlDirectorRoot.cs
@@ -1,5 +1,6 @@
 using LingoEngine.Core;
 using LingoEngine.Director.SDL2.Casts;
+using LingoEngine.Director.SDL2.Inspector;
 using LingoEngine.Director.SDL2.Stages;
 using LingoEngine.Projects;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,19 +11,23 @@ namespace LingoEngine.Director.SDL2.UI
     {
         private readonly DirSdlCastWindow _castWindow;
         private readonly DirSdlStageWindow _stageWindow;
+        private readonly DirSdlPropertyInspectorWindow _inspectorWindow;
 
         public LingoSdlDirectorRoot(LingoPlayer player, IServiceProvider services, LingoProjectSettings settings)
         {
             _castWindow = services.GetRequiredService<DirSdlCastWindow>();
             _stageWindow = services.GetRequiredService<DirSdlStageWindow>();
+            _inspectorWindow = services.GetRequiredService<DirSdlPropertyInspectorWindow>();
             _castWindow.Popup();
             _stageWindow.Popup();
+            _inspectorWindow.Popup();
         }
 
         public void Dispose()
         {
             _castWindow.Dispose();
             _stageWindow.Dispose();
+            _inspectorWindow.Dispose();
         }
     }
 }
