f71cb8bc27f889af74427ba486ac86213285871d
Emmanuel <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-08-21 10:44:14 +0200

    Fix godot and SDL test project

--- WillMoveToOwnRepo/AbstUI/Test/AbstUI.GfxVisualTest.LGodot/GfxTest.cs
+++ WillMoveToOwnRepo/AbstUI/Test/AbstUI.GfxVisualTest.LGodot/GfxTest.cs
@@ -1,4 +1,5 @@
 using AbstUI.Components;
+using AbstUI.LGodot;
 using AbstUI.LGodot.Components;
 using AbstUI.LGodot.Styles;
 using AbstUI.Styles;
@@ -13,22 +14,24 @@ public partial class GfxTest : Node
 {
     public override void _Ready()
     {
-
-        var fontManager = new AbstGodotFontManager();
-        fontManager.LoadAll();
-        var styleManager = new AbstGodotStyleManager();
+        var rootNode = new TestRootNode(this);
+        //var fontManager = new AbstGodotFontManager();
+        //fontManager.LoadAll();
+        //var styleManager = new AbstGodotStyleManager();
         var serviceCollection = new ServiceCollection();
-        ServiceProvider serviceProvider = null;
+        ServiceProvider serviceProvider = null!;
         serviceCollection
-            .AddSingleton<IAbstStyleManager>(styleManager)
-            .AddSingleton<IAbstFontManager>(fontManager)
-            .AddSingleton<GodotComponentFactory>()
-            .AddTransient<IAbstComponentFactory>(p => p.GetRequiredService<GodotComponentFactory>())
+            .WithAbstUIGodot()
+            .AddSingleton<IAbstGodotRootNode>(rootNode)
+            //.AddSingleton<IAbstStyleManager>(styleManager)
+            //.AddSingleton<IAbstFontManager>(fontManager)
+            //.AddSingleton<GodotComponentFactory>()
+            //.AddTransient<IAbstComponentFactory>(p => p.GetRequiredService<GodotComponentFactory>())
             .AddTransient<IServiceProvider>(p => serviceProvider);
-            ;
+        ;
 
         serviceProvider = serviceCollection.BuildServiceProvider();
-
+        serviceProvider.WithAbstUIGodot();
 
 
         var factory = serviceProvider.GetRequiredService<GodotComponentFactory>();
@@ -37,5 +40,15 @@ public partial class GfxTest : Node
         {
             AddChild(node);
         }
+    }
+    private class TestRootNode : IAbstGodotRootNode
+    {
+        public TestRootNode(Node rootNode)
+        {
+            RootNode = rootNode;
+        }
+
+        public Node RootNode { get; }
+
     }
 }
--- WillMoveToOwnRepo/AbstUI/Test/AbstUI.GfxVisualTest.SDL2/Program.cs
+++ WillMoveToOwnRepo/AbstUI/Test/AbstUI.GfxVisualTest.SDL2/Program.cs
@@ -1,27 +1,29 @@
 using AbstUI.SDL2;
 using AbstUI.SDL2.Components;
+using AbstUI.SDL2.Core;
 using AbstUI.SDL2.Styles;
-using AbstUI.SDL2.Windowing;
 using AbstUI.Styles;
-using AbstUI.Windowing;
 using LingoEngine.SDL2.GfxVisualTest;
 using Microsoft.Extensions.DependencyInjection;
 
 using var rootContext = new TestSdlRootComponentContext();
-var fontManager = new SdlFontManager();
-fontManager.LoadAll();
-var styleManager = new AbstStyleManager();
+//var fontManager = new SdlFontManager();
+//fontManager.LoadAll();
+//var styleManager = new AbstStyleManager();
 var serviceCollection = new ServiceCollection();
 
 serviceCollection
-    .AddSingleton<IAbstStyleManager>(styleManager)
-    .AddSingleton<IAbstFontManager>(fontManager)
-    .AddSingleton<IAbstFrameworkWindowManager, AbstSdlWindowManager>()
-    .AddSingleton<IAbstWindowManager, AbstWindowManager>()
+    .AddSingleton<ISdlRootComponentContext>(rootContext)
+    .WithAbstUISdl()
+    //.AddSingleton<IAbstStyleManager>(styleManager)
+    //.AddSingleton<IAbstFontManager>(fontManager)
+    //.AddSingleton<IAbstFrameworkWindowManager, AbstSdlWindowManager>()
+    //.AddSingleton<IAbstWindowManager, AbstWindowManager>()
     ;
 
 var serviceProvier = serviceCollection.BuildServiceProvider();
-
+serviceProvier.WithAbstUISdl();
+serviceProvier.GetRequiredService<IAbstFontManager>().LoadAll();
 var factory = new AbstSdlComponentFactory(rootContext, serviceProvier);
 rootContext.Factory = factory;
 
