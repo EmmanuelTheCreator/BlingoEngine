6285607a3826426ff06c5d0768d6f5c8bddfcba6
Emmanuel The Creator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-09-10 04:21:22 +0200

    Fix build and add director runner projects

---
+++ src/Director/LingoEngine.Director.Runner.Godot/Scenes/DirectorRunner.cs
@@ -0,0 +1,20 @@
+using Godot;
+using LingoEngine.Director.LGodot;
+using LingoEngine.Director.Host;
+using LingoEngine.Setup;
+using Microsoft.Extensions.DependencyInjection;
+
+public partial class DirectorRunner : Node
+{
+    public override void _Ready()
+    {
+        var services = new ServiceCollection();
+        services.RegisterLingoEngine(c =>
+        {
+            c.WithDirectorHostServer();
+            c.WithDirectorGodotEngine(this);
+        });
+        var sp = services.BuildServiceProvider();
+        sp.GetRequiredService<ILingoEngineRegistration>().Build(sp);
+    }
+}
