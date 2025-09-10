65772bb8cb073ead3fa9d25175ece4386bda127c
Emmanuel The Creator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-09-10 05:02:27 +0200

    Add DebugWithDirector config for SDL demo

--- Demo/TetriGrounds/LingoEngine.Demo.TetriGrounds.SDL2/Program.cs
+++ Demo/TetriGrounds/LingoEngine.Demo.TetriGrounds.SDL2/Program.cs
@@ -1,6 +1,10 @@
 ﻿using LingoEngine.SDL2;
 using LingoEngine.Setup;
 using Microsoft.Extensions.DependencyInjection;
+#if DEBUG_WITH_DIRECTOR
+using LingoEngine.Director.Core.Projects;
+using LingoEngine.Director.SDL2;
+#endif
 
 namespace LingoEngine.Demo.TetriGrounds.SDL2
 {
@@ -10,11 +14,22 @@ namespace LingoEngine.Demo.TetriGrounds.SDL2
         {
             var services = new ServiceCollection();
             IServiceProvider? serviceProvider = null;
+#if DEBUG_WITH_DIRECTOR
+            services.RegisterLingoEngine(c => c
+                    .WithDirectorSdlEngine("TetriGrounds", 1600, 970, d =>
+                    {
+                        d.CsProjFile = "LingoEngine.Demo.TetriGrounds.Core\\LingoEngine.Demo.TetriGrounds.Core.csproj";
+                    })
+                    .SetProjectFactory<LingoEngine.Demo.TetriGrounds.Core.TetriGroundsProjectFactory>()
+                    .BuildAndRunProject(sp => serviceProvider = sp)
+                    );
+#else
             services.RegisterLingoEngine(c => c
                     .WithLingoSdlEngine("TetriGrounds", 730, 547)
                     .SetProjectFactory<LingoEngine.Demo.TetriGrounds.Core.TetriGroundsProjectFactory>()
                     .BuildAndRunProject(sp => serviceProvider = sp)
                     );
+#endif
             serviceProvider?.GetRequiredService<LingoSdlRootContext>().Run();
             SdlSetup.Dispose();
         }
