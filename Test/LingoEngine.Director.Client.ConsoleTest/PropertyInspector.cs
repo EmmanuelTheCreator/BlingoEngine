46aa3984262354eec4f18febf0dcf9c41246b9b9
Emmanuel The Creator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-09-13 05:46:35 +0200

    Expand property inspector tabs

--- Test/LingoEngine.Director.Client.ConsoleTest/PropertyInspector.cs
+++ Test/LingoEngine.Director.Client.ConsoleTest/PropertyInspector.cs
@@ -13,9 +13,37 @@ internal sealed class PropertyInspector : Window
             Width = Dim.Fill(),
             Height = Dim.Fill()
         };
-        var list = BuildPropertyList(new[] { "Start", "End", "LocH", "LocV", "MemberName" });
-        _tabs.AddTab(new TabView.Tab("General", list), true);
+
+        AddTab("Sprite", new[]
+        {
+            "Lock", "FlipH", "FlipV", "Name", "X", "Y", "Z", "Left", "Top", "Right", "Bottom", "Width", "Height",
+            "Ink", "Blend", "StartFrame", "EndFrame", "Rotation", "Skew", "ForeColor", "BackColor", "Behaviors"
+        });
+        AddTab("Member", new[] { "Name", "Size", "Created", "Modified", "FileName", "Comments" });
+        AddTab("Bitmap", new[] { "Dimensions", "Highlight", "RegPointX", "RegPointY" });
+        AddTab("Sound", new[] { "Loop", "Duration", "SampleRate", "BitDepth", "Channels", "Play", "Stop" });
+        AddTab("Movie", new[]
+        {
+            "StageWidth", "StageHeight", "Resolution", "Channels", "BackgroundColor", "About", "Copyright"
+        });
+        AddTab("Cast", new[] { "Number", "Name" });
+        AddTab("Text", new[] { "Width", "Height", "Edit" });
+        AddTab("Shape", new[] { "Shape", "Filled", "Width", "Height", "Edit" });
+        AddTab("Guides", new[]
+        {
+            "GuidesColor", "GuidesVisible", "GuidesSnap", "GuidesLock", "GridColor", "GridVisible", "GridSnap",
+            "AddVerticalGuide", "AddHorizontalGuide", "RemoveGuides", "GridWidth", "GridHeight"
+        });
+        AddTab("Behavior", new[] { "Behaviors" });
+        AddTab("FilmLoop", new[] { "Framing", "Loop", "FrameCount" });
+
         Add(_tabs);
+    }
+
+    private void AddTab(string title, string[] props)
+    {
+        var list = BuildPropertyList(props);
+        _tabs.AddTab(new TabView.Tab(title, list), _tabs.Tabs.Count == 0);
     }
 
     private static ListView BuildPropertyList(string[] props)
