cfb2d4576ce00e15425251047221ab9fc12beb8d
Emmanuel The Creator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-09-15 13:24:04 +0200

    Replace reflection-based command discovery with manual registration

--- src/Director/LingoEngine.Director.Core/DirectorSetup.cs
+++ src/Director/LingoEngine.Director.Core/DirectorSetup.cs
@@ -2,19 +2,26 @@ using AbstUI.Commands;
 using AbstUI.Components;
 using AbstUI.Windowing;
 using LingoEngine.Director.Core.Bitmaps;
+using LingoEngine.Director.Core.Bitmaps.Commands;
 using LingoEngine.Director.Core.Casts;
+using LingoEngine.Director.Core.Casts.Commands;
 using LingoEngine.Director.Core.Compilers;
 using LingoEngine.Director.Core.Compilers.Commands;
 using LingoEngine.Director.Core.FileSystems;
 using LingoEngine.Director.Core.Importer;
+using LingoEngine.Director.Core.Importer.Commands;
 using LingoEngine.Director.Core.Inspector;
+using LingoEngine.Director.Core.Inspector.Commands;
 using LingoEngine.Director.Core.Projects;
+using LingoEngine.Director.Core.Projects.Commands;
 using LingoEngine.Director.Core.Scores;
 using LingoEngine.Director.Core.Scripts;
+using LingoEngine.Director.Core.Scripts.Commands;
 using LingoEngine.Director.Core.Sprites;
 using LingoEngine.Director.Core.Sprites.Behaviors;
 using LingoEngine.Director.Core.Behaviors;
 using LingoEngine.Director.Core.Stages;
+using LingoEngine.Director.Core.Stages.Commands;
 using LingoEngine.Director.Core.Texts;
 using LingoEngine.Director.Core.Tools;
 using LingoEngine.Director.Core.UI;
@@ -112,7 +119,30 @@ namespace LingoEngine.Director.Core
 
 
                     }
-                    serviceProvider.GetRequiredService<IAbstCommandManager>().DiscoverAndSubscribe(serviceProvider,typeof(DirectorSetup).Assembly);
+                    var cmdManager = serviceProvider.GetRequiredService<IAbstCommandManager>();
+                    cmdManager
+                        .Register<DirStageManager, StageChangeBackgroundColorCommand>()
+                        .Register<DirectorStageWindow, StageToolSelectCommand>()
+                        .Register<DirectorStageWindow, MoveSpritesCommand>()
+                        .Register<DirectorStageWindow, RotateSpritesCommand>()
+                        .Register<DirCastManager, CreateFilmLoopCommand>()
+                        .Register<DirectorScriptsManager, OpenScriptCommand>()
+                        .Register<LingoCodeImporterPopupHandler, OpenLingoCodeImporterCommand>()
+                        .Register<LingoCSharpConverterPopupHandler, OpenLingoCSharpConverterCommand>()
+                        .Register<DirectorProjectManager, SaveDirProjectSettingsCommand>()
+                        .Register<DirectorPropertyInspectorWindow, OpenBehaviorPopupCommand>()
+                        .Register<CompileProjectCommandHandler, CompileProjectCommand>()
+                        .Register<DirSpritesManager, ChangeSpriteRangeCommand>()
+                        .Register<DirSpritesManager, AddSpriteCommand>()
+                        .Register<DirSpritesManager, RemoveSpriteCommand>()
+                        .Register<DirectorBitmapEditWindow, PainterToolSelectCommand>()
+                        .Register<DirectorBitmapEditWindow, PainterDrawPixelCommand>()
+                        .Register<DirectorBitmapEditWindow, PainterFillCommand>()
+                        .Register<DirectorRNetServer, ConnectRNetServerCommand>()
+                        .Register<DirectorRNetServer, DisconnectRNetServerCommand>()
+                        .Register<DirectorRNetClient, ConnectRNetClientCommand>()
+                        .Register<DirectorRNetClient, DisconnectRNetClientCommand>()
+                        .Register<RNetSettingsDialog, OpenRNetSettingsCommand>();
 
                 });
             return engineRegistration;
